4c85c79417f44bed8ab14f70b6ee23d454366cdd
sergicurtu <sergi.curtu@numancia.cat>
2018-01-26 10:35:09 +0100

    Add files via upload

---
+++ www/js/app.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function() {
+    /*	
+			Declare views
+	
+			Aquí podem afegir diferents pàgines html , indicar on estan i el nom del controller ( mirar controller.js )
+	
+	*/
+    $JSView.declareView({ 
+        inici: {
+            url: '/inici',
+            template: 'views/inici.html',
+            controller: 'inici'
+        }
+    });
+     
+    /*Declare modal*/
+    $JSView.declareModal({
+        modalA: {
+            url: '/modalA',
+            template: 'views/modalA.html',
+            controller: 'modalA'
+        }
+    });
+     
+    /*designar la pàgina inicial */
+    $JSView
+        .initView('inici');
+ 
+}, false);
---
+++ www/js/controllers.js
@@ -0,0 +1,12 @@
+/*
+		CAL AFEGIR EL NOM DE LES VISTES DECLARADES A app.js
+*/
+
+$JSView.controller = {
+    inici: function(e){
+		$JSView.dataView({},e)
+	},
+    modalA: function(e){
+		$JSView.dataView({},e)	
+	}
+}
