9c1cb3e1d4346d399f52ded728a9fac32fbc9400
sergicurtu <sergi.curtu@numancia.cat>
2018-01-30 14:24:42 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,11 @@ $JSView.controller = {
     inici: function(e){
 		$JSView.dataView({},e)
 	},
+	
+    musica: function(e){
+		$JSView.dataView({},e)
+	},
+	
     modalA: function(e){
 		$JSView.dataView({},e)	
 	}
